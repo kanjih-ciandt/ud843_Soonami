3c010da77eba4203e597bf8b9357647ca8547d29
kanjih <kanjih@ciandt.com>
2017-02-15 13:33:13 -0200

    changes

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
-            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+            "http://erthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-12-01&minmagnitude=7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -162,10 +162,20 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setReadTimeout(10000 /* milliseconds */);
                 urlConnection.setConnectTimeout(15000 /* milliseconds */);
                 urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+                int statusCode = urlConnection.getResponseCode();
+
+
+                if (statusCode == 200 ) {
+                    inputStream = urlConnection.getInputStream();
+
+                    jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Message Error " + urlConnection.getResponseCode());
+                }
+
+
             } catch (IOException e) {
-                // TODO: Handle the exception
+               Log.e(LOG_TAG, e.getMessage(),e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
